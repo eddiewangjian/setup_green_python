ac2f129db4c464a35f98e200d3e6531e5b8c59c7
wangjian22 <wangjian22@baidu.com>
2019-12-16 14:42:35 +0800

    完善常用的模块包

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-PROCESS_DIR=$(pwd)
+VERSION=2.7.17
 
+PROCESS_DIR=$(pwd)
 # python压缩包的解压后路径
-python_pkg=./Python-2.7.17
+python_pkg=./Python-${VERSION}
 # python部署路径
-deploy_dir=$PROCESS_DIR/../python2717
+deploy_dir=$PROCESS_DIR/../python${VERSION}
 
 # ------------ green python pkg ------------
 cd ${python_pkg}
@@ -21,6 +22,12 @@ ${deploy_dir}/bin/pip install ConfigParser
 ${deploy_dir}/bin/pip install logging
 ${deploy_dir}/bin/pip install mysql
 ${deploy_dir}/bin/pip install tornado
+${deploy_dir}/bin/pip install protobuf
+${deploy_dir}/bin/pip install tensorflow
+${deploy_dir}/bin/pip install matplotlib
+${deploy_dir}/bin/pip install numpy
+
+${deploy_dir}/bin/python ./sofa-pbrpc/python/setup.py install
 
 
 
